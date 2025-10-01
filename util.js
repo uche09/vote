0a0d537ebbd040a8115bbd6562646624f2494c95
DonMike <Michealizzzy@gmail.com>
2025-10-01 22:13:33 +0100

    percentage

--- util.js
+++ util.js
@@ -6,3 +6,24 @@ export function incrementYes(votes) {
     }
     return votes;
 }
+
+function calVotesPercentage (Votes){
+    const yesVotes = Votes.yes || 0;
+    const noVotes = Votes.no || 0;
+    const totalVotes = yesVotes + noVotes;
+
+    if (totalVotes ===0) {
+        return {
+            yesPercentage: 0,
+            noPercentage: 0
+        };
+    }
+
+    const yesPercentage = (yesVotes / totalVotes) *100;
+    const noPercentage = (noVotes / totalVotes) *100;
+
+    return {
+        yesPercentage: yesPercentage.toFixed(2),
+        noPercentage: noPercentage.toFixed(2)
+    }
+}
